e62d5effd34ec0e3a22cbfed0d0501e7a879735b
musaclo@gmail.com <musaclo@gmail.com>
2018-12-21 10:56:22 +0200

    creation of condition for right enswer

--- js/script1.js
+++ js/script1.js
@@ -10,3 +10,44 @@ function check() {
   var question9 = document.quiz.question9.value;
   var question10 = document.quiz.question10.value;
   var question11 = document.quiz.question11.value;
+  var score;
+  var correct = 0;
+
+  if (question1 == "b") {
+    correct++;
+  }
+  if (question2 == "a") {
+    correct++;
+  }
+  if (question3 == "c") {
+    correct++;
+  }
+  if (question4 == "b") {
+    correct++;
+  }
+  if (question5 == "a") {
+    correct++;
+  }
+
+  if (question6 == "a") {
+    correct++;
+  }
+
+  if (question7 == "d") {
+    correct++;
+  }
+
+  if (question8 == "d") {
+    correct++;
+  }
+
+  if (question9 == "b") {
+    correct++;
+  }
+
+  if (question10 == "c") {
+    correct++;
+  }
+  if (question11 == "a") {
+    correct++;
+  }
